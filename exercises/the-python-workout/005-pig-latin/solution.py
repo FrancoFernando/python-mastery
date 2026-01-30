5685526bee1993181d2a43b1d05dd689d394d6d6
FrancoFernando <me@francofernando.com>
2026-01-30 10:50:04 +0100

    add solution

--- exercises/the-python-workout/005-pig-latin/solution.py
+++ exercises/the-python-workout/005-pig-latin/solution.py
@@ -1,3 +1,18 @@
 """Exercise 5: Pig Latin"""
 
-# TODO: Implement solution
+def pig_latin():
+    word = input("Enter an english word:")
+    translation = pig_latin_internal(word)
+    print(f"Pig latin translation:{translation}")
+
+def pig_latin_internal(english_word):
+
+    if len(english_word) == 0:
+        return ''
+    
+    if english_word[0] in "aeiou":
+        return f"{english_word}way"
+    else:
+        return f"{english_word[1:]}{english_word[0]}ay"
+    
+pig_latin()
