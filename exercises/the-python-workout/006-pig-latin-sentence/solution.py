65330e5fe07a34ea616fa95642ab214d4cc85e90
FrancoFernando <me@francofernando.com>
2026-02-01 22:54:05 +0100

    add solution

--- exercises/the-python-workout/006-pig-latin-sentence/solution.py
+++ exercises/the-python-workout/006-pig-latin-sentence/solution.py
@@ -1,3 +1,17 @@
 """Exercise 6: Pig Latin Sentence"""
 
-# TODO: Implement solution
+def pl_sentence():
+    sentence = input("Enter a sentence:")
+    print(pl_sentence_internal(sentence))
+
+def pl_sentence_internal(sentence):
+    pl_words = []
+    for word in sentence.split():    
+        if word[0] in "aeiou":
+            pl_words.append(f"{word}way")
+        else:
+            pl_words.append(f"{word[1:]}{word[0]}ay")
+
+    return " ".join(pl_words)
+
+pl_sentence()
