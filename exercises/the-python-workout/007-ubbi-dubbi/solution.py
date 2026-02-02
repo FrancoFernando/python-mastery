e668c1c8921c40a5e4a82fd8b06c6cfaf13aaf58
FrancoFernando <me@francofernando.com>
2026-02-02 23:48:38 +0100

    solution

--- exercises/the-python-workout/007-ubbi-dubbi/solution.py
+++ exercises/the-python-workout/007-ubbi-dubbi/solution.py
@@ -1,3 +1,16 @@
 """Exercise 7: Ubbi Dubbi"""
 
-# TODO: Implement solution
+def ubbi_dubbi():
+    word = input("Enter a word:")
+    print(f"Ubbi Dubbi translation is: {ubbi_dubbi_internal(word)}")
+
+def ubbi_dubbi_internal(word):
+    output = []
+    for c in word:
+        if c in "aeiou":
+            output.append(f"ub{c}")
+        else:
+            output.append(c)
+    return ''.join(output)
+
+ubbi_dubbi()
