96357f53c1aa5c81a60b4a7e101b37c855daea49
FrancoFernando <me@francofernando.com>
2026-02-04 22:29:17 +0100

    solution

--- exercises/the-python-workout/009-first-last/solution.py
+++ exercises/the-python-workout/009-first-last/solution.py
@@ -1,3 +1,15 @@
 """Exercise 9: First Last"""
 
-# TODO: Implement solution
+def firstlast(sequence):
+    return sequence[0::len(sequence)-1]
+
+def firstlast_v2(sequence):
+    return sequence[:1] + sequence[-1:]
+
+print(firstlast([1,2,3]))
+print(firstlast((1,2,3)))
+print(firstlast('abc'))
+
+print(firstlast_v2([1,2,3]))
+print(firstlast_v2((1,2,3)))
+print(firstlast_v2('abc'))
