0e2a89de628cd26027960d84067588cdbd7fdb7c
FrancoFernando <me@francofernando.com>
2026-02-05 22:02:49 +0100

    mysol

--- exercises/the-python-workout/010-summing-anything/solution.py
+++ exercises/the-python-workout/010-summing-anything/solution.py
@@ -1,3 +1,16 @@
 """Exercise 10: Summing Anything"""
 
-# TODO: Implement solution
+def my_sum(*args):
+
+    if not args:
+        return args
+    # args is a tuple
+    result = args[0]
+
+    for arg in args[1:]:
+        result += arg
+    return result
+
+print(my_sum(5, 10, 12, 15))
+print(my_sum([5, 10], [12, 15]))
+print(my_sum('abc','def'))
