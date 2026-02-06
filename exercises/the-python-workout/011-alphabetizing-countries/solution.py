74067797f2b9a7bf0706d07a82589d261e6a38fa
FrancoFernando <me@francofernando.com>
2026-02-06 14:51:45 +0100

    first draft

--- exercises/the-python-workout/011-alphabetizing-countries/solution.py
+++ exercises/the-python-workout/011-alphabetizing-countries/solution.py
@@ -1,3 +1,19 @@
 """Exercise 11: Alphabetizing Countries"""
 
-# TODO: Implement solution
+# this is a list of dict
+countries = [
+    {'name': 'Canada', 'size': 9984670, 'population': 38250000},
+    {'name': 'Italy', 'size': 301340, 'population': 59110000},
+    {'name': 'United Kingdom', 'size': 242495, 'population': 67220000},
+    {'name': 'France', 'size': 551695, 'population': 67390000},
+    {'name': 'Germany', 'size': 357022, 'population': 83200000},
+    {'name': 'Japan', 'size': 377975, 'population': 125700000},
+    {'name': 'United States', 'size': 9833517, 'population': 331900000}]
+
+def sorting_key(dictionary):
+    return dictionary['name']
+
+def alphabetizing_countries(countries):
+    return sorted(countries, key=sorting_key)
+
+print(alphabetizing_countries(countries))    
