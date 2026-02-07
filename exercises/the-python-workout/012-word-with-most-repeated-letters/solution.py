9b3ac3d6e1bdf3dc7c932fb33b7755f65514626a
FrancoFernando <me@francofernando.com>
2026-02-07 23:03:49 +0100

    basic solution

--- exercises/the-python-workout/012-word-with-most-repeated-letters/solution.py
+++ exercises/the-python-workout/012-word-with-most-repeated-letters/solution.py
@@ -1,3 +1,16 @@
 """Exercise 12: Word With Most Repeated Letters"""
+from collections import Counter
 
-# TODO: Implement solution
+def most_repeating_word(words):
+    output = ""
+    high_frequency = 0
+    for word in words:
+        most_repeated_value = Counter(word).most_common(1)[0][1]
+        if most_repeated_value > high_frequency:
+            output = word
+            high_frequency = most_repeated_value
+    
+    return output
+
+print(most_repeating_word(['this', 'is', 'an', 'elementary', 'test', 'example']))
+
