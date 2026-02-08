1703d4ea1c41bc6d8820d9660beff7d5cc922fbd
FrancoFernando <me@francofernando.com>
2026-02-08 23:00:08 +0100

    first version

--- exercises/the-python-workout/013-printing-tuple-records/solution.py
+++ exercises/the-python-workout/013-printing-tuple-records/solution.py
@@ -1,3 +1,21 @@
 """Exercise 13: Printing Tuple Records"""
 
-# TODO: Implement solution
+COUNTRIES = [('Canada', 9984670, 38250000),
+             ('Italy', 301340, 59110000),
+             ('United Kingdom', 242495, 67220000),
+             ('France', 551695, 67390000),
+             ('Germany', 357022, 83200000),
+             ('Japan', 377975, 125700000),
+             ('United States', 9833517, 331900000)
+]
+
+def format_sort_records():
+
+    countries_by_name = sorted(COUNTRIES, key=lambda c : c[0])
+    output = []
+    for country in countries_by_name:
+        output.append("{0:14} {1:10,} {2:14,}".format(country[0],country[1],country[2]))
+
+    return "\n".join(output)
+
+print(format_sort_records())
