3e56a51490682d6d1b088fbb8ad77675be3d6fad
FrancoFernando <me@francofernando.com>
2026-02-10 22:31:35 +0100

    better formatting

--- exercises/the-python-workout/015-rainfall/solution.py
+++ exercises/the-python-workout/015-rainfall/solution.py
@@ -1,20 +1,21 @@
 """Exercise 15: Rainfall"""
 
-def print_report(data):
-    for city,rain in data.items():
+def print_rainfall_report(rainfall_data):
+    for city,rain in rainfall_data.items():
         print(f"{city}: {rain}")
 
 def get_rainfall():
     rainfall = {}
+    print("Enter rainfall data (press Enter on city name to finish)\n")
     while True:
 
-        city = input("Enter the name of a city:")
+        city = input("City name:")
 
         if not city:
             break
 
-        rain = input("Amount of rain:")
+        rain = input(f"Rainfall in {city} (mm):")
         rainfall[city] = rain
-    print_report(rainfall)
+    print_rainfall_report(rainfall)
 
 get_rainfall()
