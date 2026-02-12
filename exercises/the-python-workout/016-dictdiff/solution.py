6473c38fa98cca9fbf50d24c298fb0b05eeecd82
FrancoFernando <me@francofernando.com>
2026-02-12 07:05:52 +0100

    first version

--- exercises/the-python-workout/016-dictdiff/solution.py
+++ exercises/the-python-workout/016-dictdiff/solution.py
@@ -1,3 +1,18 @@
 """Exercise 16: Dictdiff"""
 
-# TODO: Implement solution
+def dictdiff(first, second):
+    output = {}
+    keys = first.keys() | second.keys()
+    
+    for key in keys:
+        value_first = first.get(key)
+        value_second = second.get(key)
+        if (value_first != value_second):
+            output[key] = [value_first, value_second]
+    return output
+
+d1 = {'a':1, 'b':2, 'c':3}
+d2 = {'a':1, 'b':2, 'c':4}
+print(dictdiff(d1,d2))
+
+
