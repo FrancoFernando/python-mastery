f7916ab35154757cb70c829d40f7f711731f4881
FrancoFernando <me@francofernando.com>
2026-02-13 22:32:29 +0100

    solution

--- exercises/the-python-workout/017-how-many-different-numbers/solution.py
+++ exercises/the-python-workout/017-how-many-different-numbers/solution.py
@@ -1,3 +1,7 @@
 """Exercise 17: How Many Different Numbers"""
 
-# TODO: Implement solution
+def how_many_different_numbers(nums):
+    return len(set(nums))
+
+numbers = [1, 2, 3, 1, 2, 3, 4, 1]
+print(how_many_different_numbers(numbers))
