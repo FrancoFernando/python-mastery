c1d4eb482e36a26fcd1f4f4441751582294d415d
FrancoFernando <me@francofernando.com>
2026-02-14 22:41:13 +0100

    add solution

--- exercises/the-python-workout/018-final-line/solution.py
+++ exercises/the-python-workout/018-final-line/solution.py
@@ -1,3 +1,14 @@
 """Exercise 18: Final Line"""
 
-# TODO: Implement solution
+def get_final_line(filename):
+    last_line = ""
+    with open (filename, 'r') as file:
+        for line in file:
+            last_line = line
+    return last_line
+
+from pathlib import Path
+
+script_dir = Path(__file__).parent
+filepath = script_dir / 'demofile.txt'
+print(get_final_line(filepath))
