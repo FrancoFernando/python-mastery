fdcffe9b9e16461f629be00bed6f360d353ddb8d
FrancoFernando <me@francofernando.com>
2026-02-16 15:43:59 +0100

    better solution

--- exercises/the-python-workout/020-word-count/solution.py
+++ exercises/the-python-workout/020-word-count/solution.py
@@ -1,24 +1,29 @@
 """Exercise 20: Word Count"""
 
 def wordcount(filename):
-    characters = 0
-    number_of_words = 0
-    lines = 0
+    char_count = 0
+    words_count = 0
+    lines_count = 0
     unique_words = set()
     with open(filename) as file:
         for line in file:
-            characters += len(line)
+            char_count += len(line)
+            lines_count += 1
             words = line.split()
-            number_of_words += len(words)
-            lines += 1
+            words_count += len(words)
             unique_words.update(words)
-    
-    print(f"Characters: {characters} \n" 
-          f"Words: {number_of_words} \n"
-          f"Lines: {lines} \n"
-          f"Unique words: {len(unique_words)} \n")
 
+    stats = {
+        'characters': char_count,
+        'words': words_count,
+        'lines': lines_count,
+        'unique_words': len(unique_words)
+    }
     
+    for key, value in stats.items():
+        print(f"{key.title()}: {value}")
+
+    return stats
 
 from pathlib import Path
 
