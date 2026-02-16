5f80d1d6fb9bac6fff847ccd6b196eff745e5a12
FrancoFernando <me@francofernando.com>
2026-02-16 15:28:56 +0100

    first solution

--- exercises/the-python-workout/020-word-count/solution.py
+++ exercises/the-python-workout/020-word-count/solution.py
@@ -1,3 +1,27 @@
 """Exercise 20: Word Count"""
 
-# TODO: Implement solution
+def wordcount(filename):
+    characters = 0
+    number_of_words = 0
+    lines = 0
+    unique_words = set()
+    with open(filename) as file:
+        for line in file:
+            characters += len(line)
+            words = line.split()
+            number_of_words += len(words)
+            lines += 1
+            unique_words.update(words)
+    
+    print(f"Characters: {characters} \n" 
+          f"Words: {number_of_words} \n"
+          f"Lines: {lines} \n"
+          f"Unique words: {len(unique_words)} \n")
+
+    
+
+from pathlib import Path
+
+script_dir = Path(__file__).parent
+file_path = script_dir.joinpath("wcfile.txt")
+wordcount(file_path)
