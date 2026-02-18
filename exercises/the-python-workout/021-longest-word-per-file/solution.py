5045a01ad77e1da6880afe4c4c34c4df65406e4d
FrancoFernando <me@francofernando.com>
2026-02-18 23:02:30 +0100

    works

--- exercises/the-python-workout/021-longest-word-per-file/solution.py
+++ exercises/the-python-workout/021-longest-word-per-file/solution.py
@@ -4,6 +4,13 @@ def find_longest_word(filename):
     longest_word = ""
     with open(filename) as file:
         for line in file:
-            longest_word = max(longest_word, max(line.split(), key=len), key=len)
+            words = line.split()
+            if words:
+                longest_word = max(longest_word, max(words, key=len), key=len)
         return longest_word
 
+from pathlib import Path
+
+script_path = Path(__file__).parent
+filepath = script_path / 'files' / '43-0.txt'
+print(find_longest_word(filepath))
