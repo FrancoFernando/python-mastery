6e545e544bf01e0267bd01fc3b5f4b9df876e756
FrancoFernando <me@francofernando.com>
2026-02-19 23:01:24 +0100

    improved sol

--- exercises/the-python-workout/022-reading-and-writing-csv/solution.py
+++ exercises/the-python-workout/022-reading-and-writing-csv/solution.py
@@ -2,14 +2,16 @@
 import csv
 
 def passwd_to_csv(input_filename, output_filename):
-    with open(input_filename) as input_file, open(output_filename,'w') as output_file:
-        input = csv.reader(input_file, delimiter=':')
-        output = csv.writer(output_file, delimiter='\t')
-        for row in input:
-            print(row)
+    with open(input_filename) as input_file, \
+         open(output_filename,'w') as output_file:
+        
+        reader = csv.reader(input_file, delimiter=':')
+        writer = csv.writer(output_file, delimiter='\t')
+
+        for row in reader:
             if row and not row[0].startswith('#'):
                 username, _, id, *_ = row
-                output.writerow([username,id])
+                writer.writerow([username,id])
 
 from pathlib import Path
 
