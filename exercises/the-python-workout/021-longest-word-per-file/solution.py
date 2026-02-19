2d6e2ac91b677b8d81f6ce6b2bf60def9991e8b0
FrancoFernando <me@francofernando.com>
2026-02-19 11:23:34 +0100

    use os

--- exercises/the-python-workout/021-longest-word-per-file/solution.py
+++ exercises/the-python-workout/021-longest-word-per-file/solution.py
@@ -20,7 +20,12 @@ def find_all_longest_words(dirname):
         output[file.name] = find_longest_word(file) 
     return output"""
 
+def find_all_longest_words_v2(dirname):
+    return {file:find_longest_word(os.path.join(dirname, file)) 
+            for file in os.listdir(dirname) 
+            if os.path.isfile(os.path.join(dirname, file))}
+
 script_path = Path(__file__).parent
 filepath = script_path / 'files' / '43-0.txt'
-print(find_all_longest_words(script_path / 'files'))
+print(find_all_longest_words_v2(script_path / 'files'))
 #print(find_longest_word(filepath))
