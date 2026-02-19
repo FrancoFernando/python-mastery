b270ef60ee8c7840f73feb16f8275308efa66011
FrancoFernando <me@francofernando.com>
2026-02-19 22:51:19 +0100

    first version

--- exercises/the-python-workout/022-reading-and-writing-csv/solution.py
+++ exercises/the-python-workout/022-reading-and-writing-csv/solution.py
@@ -1,3 +1,21 @@
 """Exercise 22: Reading And Writing Csv"""
+import csv
 
-# TODO: Implement solution
+def passwd_to_csv(input_filename, output_filename):
+    with open(input_filename) as input_file, open(output_filename,'w') as output_file:
+        input = csv.reader(input_file, delimiter=':')
+        output = csv.writer(output_file, delimiter='\t')
+        for row in input:
+            print(row)
+            if row and not row[0].startswith('#'):
+                username, _, id, *_ = row
+                output.writerow([username,id])
+
+from pathlib import Path
+
+script_path = Path(__file__).parent
+input = script_path / 'passwd.txt'
+output = script_path / 'passwd_out.txt'
+passwd_to_csv(input,output)
+
+
