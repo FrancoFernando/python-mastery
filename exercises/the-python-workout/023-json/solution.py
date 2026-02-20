8c167c0a3213211aa0add08a51c0c2214feb17eb
FrancoFernando <me@francofernando.com>
2026-02-20 11:26:29 +0100

    first draft solution

--- exercises/the-python-workout/023-json/solution.py
+++ exercises/the-python-workout/023-json/solution.py
@@ -1,3 +1,11 @@
 """Exercise 23: Json"""
+import json 
+from pathlib import Path
 
-# TODO: Implement solution
+def print_scores(directory_name):
+    for file_name in Path(directory_name).iterdir():
+        with open(file_name) as file:
+            print(json.load(file))
+
+test_directory = Path(__file__).parent / "scores"
+print_scores(test_directory)
