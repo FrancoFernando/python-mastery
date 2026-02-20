be87660aa68e33817b5d34c5c66665b9bc4ad266
FrancoFernando <me@francofernando.com>
2026-02-20 23:24:20 +0100

    improved solution

--- exercises/the-python-workout/023-json/solution.py
+++ exercises/the-python-workout/023-json/solution.py
@@ -4,20 +4,30 @@ from pathlib import Path
 from collections import defaultdict
 
 def print_scores(directory_name):
+    directory = Path(directory_name)
+    if not directory.exists():
+        return
+    
+    json_files = Path(directory_name).glob('*.json')
+    if not json_files:
+        return
+    
     output = {}
-    for file_name in Path(directory_name).iterdir():
-        with open(file_name) as file:
-            output[file.name] = defaultdict(list)
+    for file_path in sorted(json_files):
+        with open(file_path) as file:
+            output[file_path.name] = defaultdict(list)
             for row in json.load(file):
                 for subject, score in row.items():
-                    output[file.name][subject].append(score)
-    #print(output)
-    for file_name, data in output.items():
-        print(file_name)
+                    output[file_path.name][subject].append(score)
+
+    print_statistics(output)
+
+def print_statistics(stats):
+    for file_path, data in stats.items():
+        print(file_path)
         for subject, scores in data.items():
-            print(f"{subject}: min {min(scores)} max {max(scores)} average {sum(scores)/len(scores)}")
-
-
+            print(f"{subject}: min {min(scores)}, max {max(scores)}," \
+                   f"average {sum(scores)/len(scores)}")
 
 test_directory = Path(__file__).parent / "scores"
 print_scores(test_directory)
