3a6a1ebaa41c6b8d6863b0bbdfbaafcdb986a778
FrancoFernando <me@francofernando.com>
2026-02-22 23:42:54 +0100

    first solution

--- exercises/the-python-workout/025-xml-generator/solution.py
+++ exercises/the-python-workout/025-xml-generator/solution.py
@@ -1,3 +1,14 @@
-"""Exercise 25: Xml Generator"""
+"""Exercise 25: Xml Generator myxml('foo') <foo></foo>
+ myxml('foo', 'bar') <foo>bar</foo>
+ myxml('foo', 'bar', a=1, b=2, c=3) <foo a="1" b="2" c="3">bar</foo>"""
 
-# TODO: Implement solution
+def myxml(name, content ="", **kwargs):
+    attributes = [f"{key}=\"{value}\"" for key, value in kwargs.items()]
+    attributes_str = " ".join(attributes)
+    opening_tag = f"<{name} {attributes_str}>"
+    closing_tag = f"</{name}>"
+    return "".join([opening_tag, content, closing_tag])
+    
+print(myxml('foo', 'bar', a=1, b=2, c=3))
+
+
