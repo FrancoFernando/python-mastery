34038f4280b9f6c2e727d9323f54e449e6922911
Fernando Franco <me@francofernando.com>
2026-02-23 16:32:56 +0100

    comment clean

--- exercises/the-python-workout/025-xml-generator/solution.py
+++ exercises/the-python-workout/025-xml-generator/solution.py
@@ -1,6 +1,4 @@
-"""Exercise 25: Xml Generator myxml('foo') <foo></foo>
- myxml('foo', 'bar') <foo>bar</foo>
- myxml('foo', 'bar', a=1, b=2, c=3) <foo a="1" b="2" c="3">bar</foo>"""
+"""Exercise 25: Xml Generator"""
 
 def myxml(tagname, content ="", **kwargs):
     attributes = " ".join([f'{key}="{value}"' for key, value in kwargs.items()])
