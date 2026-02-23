9b9d7abfdb09d21a666ebfa6bdc1c29c1e8ff3d4
Fernando Franco <me@francofernando.com>
2026-02-23 16:32:13 +0100

    better sol

--- exercises/the-python-workout/025-xml-generator/solution.py
+++ exercises/the-python-workout/025-xml-generator/solution.py
@@ -2,13 +2,8 @@
  myxml('foo', 'bar') <foo>bar</foo>
  myxml('foo', 'bar', a=1, b=2, c=3) <foo a="1" b="2" c="3">bar</foo>"""
 
-def myxml(name, content ="", **kwargs):
-    attributes = [f"{key}=\"{value}\"" for key, value in kwargs.items()]
-    attributes_str = " ".join(attributes)
-    opening_tag = f"<{name} {attributes_str}>"
-    closing_tag = f"</{name}>"
-    return "".join([opening_tag, content, closing_tag])
+def myxml(tagname, content ="", **kwargs):
+    attributes = " ".join([f'{key}="{value}"' for key, value in kwargs.items()])
+    return f"<{tagname} {attributes}>{content}</{tagname}>" 
     
 print(myxml('foo', 'bar', a=1, b=2, c=3))
-
-
