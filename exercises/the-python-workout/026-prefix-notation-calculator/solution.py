85bc51b4663f882f1546c0196d13c5ae2d554690
Fernando Franco <me@francofernando.com>
2026-02-23 17:12:25 +0100

    solution

--- exercises/the-python-workout/026-prefix-notation-calculator/solution.py
+++ exercises/the-python-workout/026-prefix-notation-calculator/solution.py
@@ -1,3 +1,14 @@
 """Exercise 26: Prefix Notation Calculator"""
 
-# TODO: Implement solution
+dispatch_table = {"+": lambda x,y : x+y, 
+                  "-": lambda x,y : x-y,
+                  "*": lambda x,y : x*y,
+                  "/": lambda x,y : x/y,
+                  "%": lambda x,y : x%y,
+                  "**": lambda x,y : x**y,}
+
+def calc(prefix_expression):
+    operator, left, right = prefix_expression.split(maxsplit=2)
+    return dispatch_table[operator](int(left), int(right))
+
+print(calc("** 2 3"))
