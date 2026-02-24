10e2f1131a7863ca93368db529b849ea7d04a7c2
FrancoFernando <me@francofernando.com>
2026-02-24 23:37:30 +0100

    solution

--- exercises/the-python-workout/027-password-generator/solution.py
+++ exercises/the-python-workout/027-password-generator/solution.py
@@ -1,3 +1,13 @@
 """Exercise 27: Password Generator"""
+import random
 
-# TODO: Implement solution
+def create_password_generator(valid_chars):
+
+    def password_generator(length):
+        psw = [valid_chars[random.randint(0,len(valid_chars)-1)] for _ in range(length)]
+        return "".join(psw)
+    
+    return password_generator
+
+alpha_password = create_password_generator('abcdef')
+print(alpha_password(5)) 
