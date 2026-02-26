440726b9c8b6211297e3acb8b78cca9eec6874d9
FrancoFernando <me@francofernando.com>
2026-02-26 22:27:43 +0100

    solution

--- exercises/the-python-workout/029-add-numbers/solution.py
+++ exercises/the-python-workout/029-add-numbers/solution.py
@@ -1,3 +1,6 @@
 """Exercise 29: Add Numbers"""
 
-# TODO: Implement solution
+def sum_numbers(input):
+    return sum([int(x) for x in input.split() if str.isdigit(x)])
+
+print(sum_numbers("10 abc 20 de44 30 55fg 40"))
