358d088992ce634c217f075ce655a9b2855422d7
FrancoFernando <me@francofernando.com>
2026-02-28 11:06:08 +0100

    solution

--- exercises/the-python-workout/031-pig-latin-translation-of-a-file/solution.py
+++ exercises/the-python-workout/031-pig-latin-translation-of-a-file/solution.py
@@ -1,3 +1,20 @@
 """Exercise 31: Pig Latin Translation Of A File"""
 
-# TODO: Implement solution
+def pig_latin(word):
+    
+    if word[0] in "aeiou":
+        return f"{word}way"
+    else:
+        return f"{word[1:]}{word[0]}ay"
+
+def plfile(filename):
+    with open(filename) as file:
+        return ' '.join(pig_latin(word)
+                         for line in file
+                         for word in line.split())
+    
+from pathlib import Path
+
+script_path = Path(__file__).parent
+file_path = script_path / "demofile.txt"
+print(plfile(file_path))
