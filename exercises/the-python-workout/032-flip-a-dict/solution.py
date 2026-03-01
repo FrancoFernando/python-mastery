9a17dc6ec0db587ae539bc425b3d0257277f3b6c
FrancoFernando <me@francofernando.com>
2026-03-01 23:02:03 +0100

    sol

--- exercises/the-python-workout/032-flip-a-dict/solution.py
+++ exercises/the-python-workout/032-flip-a-dict/solution.py
@@ -1,3 +1,8 @@
 """Exercise 32: Flip A Dict"""
 
-# TODO: Implement solution
+def flipped_dict(input_dict):
+    return {value:key
+            for key, value in input_dict.items()}
+
+a_dict = {'a':1,'b':2,'c':3}
+print(flipped_dict(a_dict))
