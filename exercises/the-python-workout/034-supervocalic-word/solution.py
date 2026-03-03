823f67254e320b54fbbd11f3ddd5e30b726df232
FrancoFernando <me@francofernando.com>
2026-03-03 22:25:41 +0100

    sol

--- exercises/the-python-workout/034-supervocalic-word/solution.py
+++ exercises/the-python-workout/034-supervocalic-word/solution.py
@@ -1,3 +1,19 @@
 """Exercise 34: Supervocalic Word"""
+from collections import Counter
 
-# TODO: Implement solution
+def get_sv(filename):
+
+    vocals = {'a','e','i','o','u'}
+       
+    with open(filename) as file:
+        return {word 
+                for word in file
+                if vocals < set(word)}
+    
+from pathlib import Path
+script_path = Path(__file__).parent
+file_path = script_path / "words.txt"
+print(get_sv(file_path))
+
+
+
