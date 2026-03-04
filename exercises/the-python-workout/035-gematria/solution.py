fbd54c01166fb0d06c60d1ee6519bdf2af743e06
FrancoFernando <me@francofernando.com>
2026-03-04 22:12:18 +0100

    v2

--- exercises/the-python-workout/035-gematria/solution.py
+++ exercises/the-python-workout/035-gematria/solution.py
@@ -1,7 +1,14 @@
 """Exercise 35: Gematria"""
 
+import string
+
 def gematria_dict():
     return {num+1:chr(ord('a')+num) 
             for num in range(26)}
 
+def gematria_dict_v2():
+    return {num:char 
+            for char, num in enumerate(string.ascii_lowercase,1)}
+
 print(gematria_dict())
+print(gematria_dict_v2())
