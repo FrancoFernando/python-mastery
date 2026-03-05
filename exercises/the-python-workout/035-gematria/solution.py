69b91c5ea88026453f4065cba8536cf6efaf4d3c
FrancoFernando <me@francofernando.com>
2026-03-05 23:05:00 +0100

    sol

--- exercises/the-python-workout/035-gematria/solution.py
+++ exercises/the-python-workout/035-gematria/solution.py
@@ -1,5 +1,6 @@
 """Exercise 35: Gematria"""
 
+from pathlib import Path
 import string
 
 def gematria_dict():
@@ -21,7 +22,14 @@ def gematria_for_v2(word):
     d = gematria_dict()
     return sum(d[c] for c in word)
 
+def gematria_equal_words(word):
+    score = gematria_for(word.lower())
+    return [w.strip() 
+            for w in open(Path(__file__).parent / 'words.txt')
+            if gematria_for(w.strip().lower()) == score]
+
 print(gematria_dict())
 print(gematria_dict_v2())
 print(gematria_for('cat'))
 print(gematria_for_v2('cat'))
+print(gematria_equal_words('cat'))
