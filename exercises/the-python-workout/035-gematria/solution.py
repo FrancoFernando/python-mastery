ff928e8a09905ce14b753cf96efaa94c2215602c
FrancoFernando <me@francofernando.com>
2026-03-05 22:27:08 +0100

    fix dict

--- exercises/the-python-workout/035-gematria/solution.py
+++ exercises/the-python-workout/035-gematria/solution.py
@@ -3,12 +3,20 @@
 import string
 
 def gematria_dict():
-    return {num+1:chr(ord('a')+num) 
+    return {chr(ord('a')+num):num+1
             for num in range(26)}
 
 def gematria_dict_v2():
-    return {num:char 
+    return {char:num
             for char, num in enumerate(string.ascii_lowercase,1)}
+
+def gematria_for(word):
+    d = gematria_dict()
+    result = 0
+    for c in word:
+        result += d[c]
+    return result
 
 print(gematria_dict())
 print(gematria_dict_v2())
+print(gematria_for('cat'))
