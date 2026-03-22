743ee0fd617d6858b6300276f9dc28d881c56209
FrancoFernando <me@francofernando.com>
2026-03-22 22:32:45 +0100

    draft solution

--- exercises/the-python-workout/039-ice-cream-bowl/solution.py
+++ exercises/the-python-workout/039-ice-cream-bowl/solution.py
@@ -1,3 +1,16 @@
 """Exercise 39: Ice Cream Bowl"""
 
-# TODO: Implement solution
+class Scoop():
+    def __init__(self, name):
+        self.name = name
+
+class Bowl():
+    pass
+
+s1 = Scoop('chocolate')
+s2 = Scoop('vanilla')
+s3 = Scoop('persimmon')
+b = Bowl()
+b.add_scoops(s1, s2)
+b.add_scoops(s3)
+print(b)
