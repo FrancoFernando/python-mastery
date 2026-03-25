38ebf0845b21e2a29b7808182bc72abc5900d54b
FrancoFernando <me@francofernando.com>
2026-03-25 22:16:40 +0100

    solution

--- exercises/the-python-workout/041-a-bigger-bowl/solution.py
+++ exercises/the-python-workout/041-a-bigger-bowl/solution.py
@@ -1,3 +1,36 @@
 """Exercise 41: A Bigger Bowl"""
 
-# TODO: Implement solution
+"""Exercise 40: Bowl Limits"""
+
+"""Exercise 39: Ice Cream Bowl"""
+
+class Scoop():
+    def __init__(self, name):
+        self.name = name
+
+class Bowl():
+    max_scoops = 3
+    def __init__(self):
+        self.scoops = []
+
+    def __str__(self):
+        return ",".join(scoop.name for scoop in self.scoops)
+
+    def add_scoops(self, *scoops):
+        for scoop in scoops:
+            if len(self.scoops) < self.max_scoops:
+                self.scoops.append(scoop)
+
+class BigBowl(Bowl):
+    max_scoops = 5
+
+s1 = Scoop('chocolate')
+s2 = Scoop('vanilla')
+s3 = Scoop('persimmon')
+b = BigBowl()
+b.add_scoops(s1, s2)
+b.add_scoops(s3)
+b.add_scoops(s1) # added
+print(b)
+
+
